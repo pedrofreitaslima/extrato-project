f1e875695d1a9fc49378283274179549590eac04
Pedro Freitas Lima <pedrofreitaslima@brq.com>
2025-01-28 12:43:14 -0300

    CHORE: add MSK serverless cluster

---
+++ infra/msk.tf
@@ -0,0 +1,24 @@
+resource "aws_msk_serverless_cluster" "extrato_lancamento_msk_serverless_cluster" {
+  cluster_name = "${local.domain_name}-cluster"
+
+  vpc_config {
+    subnet_ids         = [
+      aws_subnet.extrato_lancamento_public_subnet_1.id,
+      aws_subnet.extrato_lancamento_public_subnet_2.id,
+      aws_subnet.extrato_lancamento_public_subnet_3.id
+    ]
+    security_group_ids = [
+      aws_security_group.extrato_lancamento_sg.id
+    ]
+  }
+
+  client_authentication {
+    sasl {
+      iam {
+        enabled = true
+      }
+    }
+  }
+
+  tags = local.custom_tags
+}
