99d46f9ec83f92a73e583d4de4cf4bb5939f685a
Pedro Freitas Lima <pedrofreitaslima@brq.com>
2025-01-28 12:40:01 -0300

    CHORE: add S3 buckets for glue

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -89,7 +89,29 @@ output "extrato_lancamento_sg_egress_all_arn" {
 #######################################################################################################################
 #### Security
 #######################################################################################################################
-output "extrato_lancamento_iam_role_arn" {
+output "extrato_lancamento_msk_ec2client_role_arn" {
   value       = aws_iam_role.extrato_lancamento_msk_ec2client_role.arn
   description = "The ARN of the iam role dedicated to project Extrato Lancamento"
+}
+
+output "extrato_lancamento_glue_msk_secleanup_role_arn" {
+  value       = aws_iam_role.extrato_lancamento_glue_msk_secleanup_role.arn
+  description = "The ARN of the iam role dedicated to project Extrato Lancamento for s3 clean up"
+}
+output "extrato_lancamento_glue_msk_getbroker_role_arn" {
+  value       = aws_iam_role.extrato_lancamento_glue_msk_getbroker_role.arn
+  description = "The ARN of the iam role dedicated to project Extrato Lancamento for get broker msk"
+}
+
+#######################################################################################################################
+#### S3 Buckets
+#######################################################################################################################
+output "extrato_lancamento_s3_bucket_glueoutput_arn" {
+  value       = aws_s3_bucket.extrato_lancamento_glueoutput_bucket.arn
+  description = "The ARN of the S3 bucket dedicated to project Extrato Lancamento for glue output"
+}
+
+output "extrato_lancamento_gluescript_bucket" {
+  value       = aws_s3_bucket.extrato_lancamento_glueoutput_bucket.arn
+  description = "The ARN of the S3 bucket dedicated to project Extrato Lancamento for glue script"
 }
---
+++ infra/s3.tf
@@ -0,0 +1,31 @@
+#######################################################################################################################
+#### S3 Buckets
+#######################################################################################################################
+resource "aws_s3_bucket" "extrato_lancamento_glueoutput_bucket" {
+  bucket = "aws-glueoutput-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.id}-${local.domain_name}"
+  tags = local.custom_tags
+}
+
+resource "aws_s3_bucket" "extrato_lancamento_gluescript_bucket" {
+  bucket = "aws-gluescript-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.id}-${local.domain_name}"
+  tags = local.custom_tags
+}
+
+#######################################################################################################################
+#### Public Access Block
+#######################################################################################################################
+resource "aws_s3_bucket_public_access_block" "extrato_lancamento_glueoutput_public_access_block" {
+  bucket                  = aws_s3_bucket.extrato_lancamento_glueoutput_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_public_access_block" "extrato_lancamento_gluescript_public_access_block" {
+  bucket                  = aws_s3_bucket.extrato_lancamento_gluescript_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
